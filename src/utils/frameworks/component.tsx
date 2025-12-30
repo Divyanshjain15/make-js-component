e41518a69d14dbce82859099350058b591901822
Divyanshjain15 <146543679+Divyanshjain15@users.noreply.github.com>
2025-12-30 16:18:21 +0530

    Update component.tsx

--- src/utils/frameworks/component.tsx
+++ src/utils/frameworks/component.tsx
@@ -1,1 +1,11 @@
+import { Component } from "solid-js";
 
+const {{componentName}}: Component = () => {
+  return (
+    <div>
+      Hello from {{componentName}} (SolidJS)
+    </div>
+  );
+};
+
+export default {{componentName}};
