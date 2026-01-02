117042f6dcadcc801d441f783bec11a54d0fe4c2
Divyanshjain15 <jaindivyansh1505gmail.com>
2026-01-02 21:14:57 +0530

    Add SolidJS component templates

---
+++ src/utils/frameworks/solidjs/component.tsx
@@ -0,0 +1,19 @@
+export const MyComponentTemplate = `
+import { Component } from "solid-js";
+
+const MyComponent: Component = () => {
+  return <div>Hello SolidJS</div>;
+};
+
+export default MyComponent;
+`;
+
+export const ButtonTemplate = `
+import { Component } from "solid-js";
+
+const Button: Component<{ label: string }> = (props) => {
+  return <button>{props.label}</button>;
+};
+
+export default Button;
+`;
